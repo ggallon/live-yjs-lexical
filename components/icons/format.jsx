f1dd8b5e4addcb25d3a9c10793cb78e2876c3156
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-02-01 21:17:38 +0100

    Extract and optimize Toolbar Icons

---
+++ components/icons/format.jsx
@@ -0,0 +1,53 @@
+export function BoldIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      aria-hidden="true"
+      viewBox="0 0 32 32"
+    >
+      <path
+        fill="currentColor"
+        d="M18.25 25H9V7h8.5a5.25 5.25 0 0 1 4 8.65A5.25 5.25 0 0 1 18.25 25ZM12 22h6.23a2.249 2.249 0 1 0 0-4.5H12V22Zm0-7.5h5.5a2.249 2.249 0 1 0 0-4.5H12v4.5Z"
+      />
+    </svg>
+  );
+}
+
+export function ItalicIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      aria-hidden="true"
+      viewBox="0 0 32 32"
+    >
+      <path
+        fill="currentColor"
+        d="M25 9V7H12v2h5.14l-4.37 14H7v2h13v-2h-5.14l4.37-14H25Z"
+      />
+    </svg>
+  );
+}
+
+export function UnderlineIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      aria-hidden="true"
+      viewBox="0 0 32 32"
+    >
+      <path
+        fill="currentColor"
+        d="M4 26h24v2H4v-2Zm12-3a7 7 0 0 1-7-7V5h2v11a5 5 0 1 0 10 0V5h2v11a7 7 0 0 1-7 7Z"
+      />
+    </svg>
+  );
+}
